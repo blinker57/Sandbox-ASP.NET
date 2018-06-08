62d53bae41ea52eec07cec36501d1c858004ee09
blinker57 <bruce@linkerhouse.com>
2018-06-08 13:49:12 -0400

    Add project files.

---
+++ Sandbox-ASP.NET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sandbox_ASP.NET.MvcApplication" Language="C#" %>
---
+++ Sandbox-ASP.NET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sandbox_ASP.NET.Startup))]
+namespace Sandbox_ASP.NET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
