8b26658eaa0db8eac3a032dee9636181d4cde837
blinker57 <bruce@linkerhouse.com>
2018-06-09 15:57:14 -0400

    Created Add a TV Show form

---
+++ Sandbox-ASP.NET/Controllers/TVShowsController.cs
@@ -0,0 +1,27 @@
+﻿using Sandbox_ASP.NET.Models;
+using Sandbox_ASP.NET.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Sandbox_ASP.NET.Controllers
+{
+  public class TVShowsController : Controller
+  {
+    private readonly ApplicationDbContext _context;
+
+    public TVShowsController()
+    {
+      _context = new ApplicationDbContext();
+    }
+
+    public ActionResult Create()
+    {
+      var viewModel = new TVShowViewModel
+      {
+        Genres = _context.Genres.ToList()
+      };
+
+      return View(viewModel);
+    }
+  }
+}
---
+++ Sandbox-ASP.NET/ViewModels/TVShowViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+using Sandbox_ASP.NET.Models;
+using System.Collections.Generic;
+
+namespace Sandbox_ASP.NET.ViewModels
+{
+  public class TVShowViewModel
+  {
+    public string Network { get; set; }
+    public string Date { get; set; }
+    public string Time { get; set; }
+    public int Genre { get; set; }
+    public IEnumerable<Genre> Genres { get; set; }
+  }
+}
