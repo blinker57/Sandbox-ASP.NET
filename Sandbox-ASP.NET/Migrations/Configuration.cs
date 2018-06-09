31372f135a3bc878db2715a91239265898f4424a
blinker57 <bruce@linkerhouse.com>
2018-06-09 12:49:53 -0400

    Enabled migrations

---
+++ Sandbox-ASP.NET/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Sandbox_ASP.NET.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Sandbox_ASP.NET.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Sandbox_ASP.NET.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
