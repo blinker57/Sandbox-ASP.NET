936dd38cb8def458a66b92d98a5489620cae6a65
blinker57 <bruce@linkerhouse.com>
2018-06-10 17:39:44 -0400

    Added Name to User class

--- Sandbox-ASP.NET/Controllers/HomeController.cs
+++ Sandbox-ASP.NET/Controllers/HomeController.cs
@@ -1,30 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Sandbox_ASP.NET.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Sandbox_ASP.NET.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private ApplicationDbContext _context;
+
+    public HomeController()
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+      _context = new ApplicationDbContext();
     }
+
+    public ActionResult Index()
+    {
+      var upcomingShows = _context.TVShows
+            .Include(tv => tv.TVWatcher)
+            .Where(tv => tv.DateTime > DateTime.Now);
+
+      return View(upcomingShows);
+    }
+
+    public ActionResult About()
+    {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+  }
 }
